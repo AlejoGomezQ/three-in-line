a9f964cdbc9fe6d94593cd8ea73ead18c7304d44
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2024-03-07 19:44:37 -0500

    board painted

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
+interface Turn {
+  X: string;
+  O: string;
+}
+
+const TURNS: Turn = {
+  X: "X",
+  O: "O",
+};
+
+const Square = ({ children, updateBoard, index }) => {
+  return <div className="square">{children}</div>;
+};
+
 function App() {
-  return <></>;
+  const [board, setBoard] = useState<(string | null)[]>(Array(9).fill(null));
+
+  const [turn, setTurn] = useState<Turn>(TURNS);
+
+  return (
+    <main className="board">
+      <h1>TRIKI</h1>
+      <section className="game">
+        {board.map((_, index: number) => {
+          return (
+            <Square
+              key={index}
+              index={index}
+              children={undefined}
+              updateBoard={undefined}
+            />
+          );
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={}>{TURNS.X}</Square>
+        <Square>{TURNS.O}</Square>
+      </section>
+    </main>
+  );
 }
 
 export default App;
